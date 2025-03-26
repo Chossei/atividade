afea777703bc831f453820c7eb28d8db296148bc
Átila Prudente <150717036+Chossei@users.noreply.github.com>
2025-03-26 08:34:28 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(page_title = 'Atividade 1 - Análise de Regressão',
+layout = 'centered', initial_side_bar_state = 'expanded')
+
+paginas = {
+'Atividades': [st.Page('paginas/correlacao.py', title = 'Correlação', default = True]
+}
+
+pag = st.navigation(paginas)
+pag.run()

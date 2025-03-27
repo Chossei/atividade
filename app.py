dd0c83290ca3a6458bca570d1c6a0c5c37c40675
Átila Prudente <150717036+Chossei@users.noreply.github.com>
2025-03-27 10:11:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import streamlit as st
 st.set_page_config(page_title='Atividade 1 - Análise de Regressão', layout='centered', initial_sidebar_state='expanded')
 
 paginas = {
-    'Atividades': [st.Page('paginas/correlacao.py', title='Correlação', default=True),
-                  st.Page('paginas/tarefa_m.py', title = 'Tarefa de amostragem (março)', default=False)]
+    'Análise de Regressão (MAT229)': [st.Page('paginas/correlacao.py', title='Correlação', default=True)]
+    'Amostragem (MATD44)': [st.Page('paginas/tarefa_m.py', title = 'Tarefa de amostragem (março)', default=False)]
 }
 
 pag = st.navigation(paginas)
